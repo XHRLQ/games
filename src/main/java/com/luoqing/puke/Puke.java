4bcbfb31d11bce67e78f121dc19d7d8a65922048
mashuai@hudongpai.com <ms120190>
2017-08-27 01:22:34 +0800

    Sma: 1. 添加依赖,修改pom.xml打包功能

--- src/main/java/com/luoqing/puke/Puke.java
+++ src/main/java/com/luoqing/puke/Puke.java
@@ -13,12 +13,11 @@ public class Puke extends Game {
 
     /**
      * 方法不要一直使用 Public static ,如果不给外部调用就用 protected/private
-     *
      */
     private List<String> puke;
 
     @Override
-    protected void startGame(){
+    protected void startGame() {
         createPuke();
         handOut();
     }
@@ -48,6 +47,7 @@ public class Puke extends Game {
         Collections.shuffle(puke);
         // todo  优化 StringBuilder/StringBuffer
         System.out.println("******发牌结果******：");
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= puke.size(); i++) {
             System.out.print(puke.get(i - 1) + " ");
             if (i % 18 == 0) {
